08c365fc883afe059fb1b821f219aa4d5c4750f4
Tsvetislav Todorov <50547533+tsvetislavt99@users.noreply.github.com>
2022-12-13 10:16:27 +0200

    Update remove-bitmap-tests-on-success.js

--- remove-bitmap-tests-on-success.js
+++ remove-bitmap-tests-on-success.js
@@ -3,8 +3,12 @@ const path = require("path");
 
 const BITMAP_TESTS = path.join(__dirname, "../bitmaps_test");
 
+/**
+ * @description This function reads the config.js file in the html_report folder and parses the JSON object after backstop has finished running.
+ * If all tests have passed, it will delete the bitmaps_test folder.
+ * If some tests have failed, it will delete the test images that have passed and the report.json files from each bitmaptest_directory. Failed test and diff images will be kept.
+ */
 function removeBitmapTestsOnSuccess() {
-  
   setTimeout(async () => {
     const configJsFile = await fs.readFile(
       path.join(__dirname, "../html_report/config.js"),
@@ -19,7 +23,6 @@ function removeBitmapTestsOnSuccess() {
     const isSuccess = testObjects.every((testObject) => testObject.status === "pass");
 
     if (isSuccess) {
-
       try {
         await fs.rm(BITMAP_TESTS, { recursive: true }, (err) => {
           if (err) {
@@ -31,7 +34,6 @@ function removeBitmapTestsOnSuccess() {
       }
     }else{
       try{
-        const failedTestObjects = testObjects.filter((testObject) => testObject.status !== "pass");
         const bitmapTests = await fs.readdir(BITMAP_TESTS);
 
         bitmapTests.forEach(async (bitmapTest) => {
@@ -39,25 +41,24 @@ function removeBitmapTestsOnSuccess() {
           const currDirFiles = await fs.readdir(currDir);
 
           currDirFiles.forEach(async (file) => {
-
-            failedTestObjects.forEach(async failedTest =>{
-              if(!failedTest.filename.includes(file) && failedTest.diff && !failedTest.diff.includes(file)){
-                await fs.unlink(path.join(currDir, file));
+            testObjects.forEach(async (testObject) => {
+              // Handles deleting the test image on tests that succeeded and the report.json file
+              if((testObject.filename.includes(file) && testObject.status === 'pass') || file === 'report.json'){
+                await fs.rm(path.join(currDir, file));
               }
-            })
-            
-          });
+            });
 
+          });
           if(!currDirFiles.length){
             await fs.rmdir(currDir);
           }
         });
-      
+
 
       }catch(err){
         console.log(`❌: ${err}`);
       }
-      
+
     }
   }, 1000);
 }
